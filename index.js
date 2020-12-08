3ded353c9c88aafb2c0ba23aad21bade8eeb3709
Solar <xviii.solar@gmail.com>
2020-12-08 17:54:46 -0500

    Bug fixing.

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ function random_from_array(images){
   return images[Math.floor(Math.random() * images.length)];
 }
 
-function upload_random_image(images){
+function tweetRandomImage(images){
   console.log('Opening an image...');
   var image_path = path.join('./images/' + random_from_array(images));
   var b64content = fs.readFileSync(image_path, { encoding: 'base64' });
