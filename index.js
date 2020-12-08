db31246405f716b2b2b40947bd0ed95919499927
Solar <xviii.solar@gmail.com>
2020-12-08 15:47:06 -0500

    Initial commit!

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ var config = require(path.join(__dirname, 'config.js'));
 var T = new Twitter({
 	consumer_key: process.env.CONSUMER_KEY,
 	consumer_secret: process.env.CONSUMER_SECRET,
-	access_token_key: process.env.ACCESS_TOKEN_KEY,
+	access_token: process.env.ACCESS_TOKEN_KEY,
 	access_token_secret: process.env.ACCESS_TOKEN_SECRET
 });
 
