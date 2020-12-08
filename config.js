87c0fbd8f8933c679725b72b9102f05e60b37dfa
Solar <xviii.solar@gmail.com>
2020-12-08 15:32:11 -0500

    Initial commit!

---
+++ config.js
@@ -0,0 +1,8 @@
+const config = {
+  consumer_key:'OIl5DCaHi9zkEOng6FQR0mjsK',
+  consumer_secret:'BJiQZr5QWWyAXVDys9o2OSBddWMQDtM4EzR2YNYwybAN3Q1vuI',
+  access_token:'1336397133046370304-QN3aGFr0k4XQ8uKKD78YFddqk8lDHl',
+  access_token_secret:'jKtaJUvUk4vPfPQ0TxNKPALExyXKjPQpGRY7HtXtXhxYD'
+}
+
+module.exports = config;
---
+++ index.js
@@ -0,0 +1,48 @@
+var Twit = require('twit');
+var fs = require('fs');
+var path = require('path');
+var config = require(path.join(__dirname, 'config.js'));
+
+var T = new Twitter({
+	consumer_key: process.env.CONSUMER_KEY,
+	consumer_secret: process.env.CONSUMER_SECRET,
+	access_token_key: process.env.ACCESS_TOKEN_KEY,
+	access_token_secret: process.env.ACCESS_TOKEN_SECRET
+});
+
+function random_from_array(images){
+  return images[Math.floor(Math.random() * images.length)];
+}
+
+function upload_random_image(images){
+  console.log('Opening an image...');
+  var image_path = path.join(__dirname, '/images/' + random_from_array(images)),
+      b64content = fs.readFileSync(image_path, { encoding: 'base64' });
+
+  console.log('Uploading an image...');
+
+  T.post('media/upload', { media_data: b64content }, function (err, data, response) {
+    if (err){
+      console.log('ERROR:');
+      console.log(err);
+    }
+    else{
+      console.log('Image uploaded!');
+      console.log('Now tweeting it...');
+
+      T.post('statuses/update', {
+        media_ids: new Array(data.media_id_string)
+      },
+      function(err, data, response) {
+        if (err){
+          console.log('ERROR:');
+          console.log(err);
+        }
+        else{
+          console.log('Posted an image!');
+        }
+      }
+    );
+  }
+});
+}
